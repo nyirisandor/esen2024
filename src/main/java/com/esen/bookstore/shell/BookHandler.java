26907d0c3d17ebc56546a4a90a480ca5c0d41cb2
Nyíri Sándor <nyiri.sandor@mailbox.unideb.hu>
2024-04-23 09:30:50 +0200

    added delete and update commands

--- src/main/java/com/esen/bookstore/shell/BookHandler.java
+++ src/main/java/com/esen/bookstore/shell/BookHandler.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 import java.util.stream.Collectors;
 
@@ -33,4 +34,18 @@ public class BookHandler {
                         .formatted(book.getId(),book.getPublisher(),book.getAuthor(),book.getTitle(),book.getPrice()))
                 .collect(Collectors.joining(System.lineSeparator()));
     }
+
+    @ShellMethod(value = "Delete book", key = "delete book")
+    void deleteBook(Long id){
+        bookService.deleteBook(id);
+    }
+
+     @ShellMethod(value = "Update book", key = "update book")
+    void updateBook(Long id,
+                    @ShellOption(defaultValue = ShellOption.NULL) String title,
+                    @ShellOption(defaultValue = ShellOption.NULL) String author,
+                    @ShellOption(defaultValue = ShellOption.NULL) String publisher,
+                    @ShellOption(defaultValue = ShellOption.NULL) Double price){
+        bookService.updateBook(id,title,author,publisher,price);
+     }
 }
